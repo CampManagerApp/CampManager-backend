391aa8b25c9e9e85b96c3cc591f75e7cae7ea9be
Galajat3x <joelaumedes@gmail.com>
2022-10-10 20:23:54 +0200

    Initialized repo

---
+++ src/main/java/camp/CampManager/CampManagerApplication.java
@@ -0,0 +1,13 @@
+package camp.CampManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/camp/CampManager/CampManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package camp.CampManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
