f2212670f702bd4878d9c463b5a80e6b8fb796cb
alejandroclavera <alejandroclavera@gmail.com>
2022-10-17 14:16:52 +0200

    Append cross origin in the backend to work with the front-end

--- src/main/java/camp/CampManager/organisation/OrganisationEndpoint.java
+++ src/main/java/camp/CampManager/organisation/OrganisationEndpoint.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @Controller
 @RequestMapping(value = "/organisation/")
 public class OrganisationEndpoint {
