b7d6a2cc737bc9cdb9db953e2e33eaab2ae4c4bb
Galajat3x <joelaumedes@gmail.com>
2022-12-02 18:43:23 +0100

    created small example of jobs WORKING

--- src/main/java/camp/CampManager/CampManagerApplication.java
+++ src/main/java/camp/CampManager/CampManagerApplication.java
@@ -2,13 +2,24 @@ package camp.CampManager;
 
 //import camp.CampManager.users.UserService;
 
+import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CampManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CampManagerApplication.class, args);
+    }
+
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper) {
+        InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(jobMapper);
+        return storageProvider;
     }
 }
---
+++ src/main/java/camp/CampManager/organisation/campaign/tables/TableEndpoint.java
@@ -0,0 +1,74 @@
+package camp.CampManager.organisation.campaign.tables;
+
+import lombok.AllArgsConstructor;
+import org.jobrunr.jobs.lambdas.JobLambda;
+import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/organisation/") // /organisation/{id}/campaign/{id}
+public class TableEndpoint {
+
+    @Autowired
+    private JobScheduler scheduler;
+
+    private String mockResult = "Nothing yet";
+    private Date jobStartTime = null;
+    private Date jobFinishTime = null;
+
+    @GetMapping(path = "/{orgId}/campaign/{campId}/init-job")
+    @ResponseBody
+    public ResponseEntity<String> startJob(@PathVariable("orgId") Long orgId,
+                                           @PathVariable("campId") Long campId) {
+        mockResult = "Job started";
+        jobStartTime = new Date();
+        System.out.println("STARTING JOB");
+        scheduler.enqueue(this::doJob);
+        System.out.println("JOB ENQUEUED");
+        return ResponseEntity.ok("Started");
+    }
+
+    public void doJob(){
+        System.out.println("JOB BEGIN");
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException ignored) {
+        }
+        System.out.println("JOB FINISHED");
+        mockResult = "Job finished";
+        jobFinishTime = new Date();
+    }
+
+    @GetMapping(path = "/{orgId}/campaign/{campId}/job-status")
+    @ResponseBody
+    public ResponseEntity<JobInformation> checkJobStatus(@PathVariable("orgId") Long orgId,
+                                                 @PathVariable("campId") Long campId) {
+        return ResponseEntity.ok(new JobInformation(jobStartTime, jobFinishTime, mockResult));
+    }
+
+    @PostMapping(path = "/{orgId}/campaign/{campId}/reset")
+    @ResponseBody
+    public ResponseEntity<JobInformation> resetToStart(@PathVariable("orgId") Long orgId,
+                                                       @PathVariable("campId") Long campId) {
+        mockResult = "Reset";
+        jobFinishTime = null;
+        jobStartTime = null;
+        return ResponseEntity.ok(new JobInformation(null, null, mockResult));
+    }
+
+    @AllArgsConstructor
+    private static class JobInformation {
+        public Date jobStartTime;
+        public Date jobFinishTime;
+        public String result;
+    }
+}
