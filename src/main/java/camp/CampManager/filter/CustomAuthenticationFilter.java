6bf33477630da17115344084b670d9931985af07
Joel Aumedes <56357800+Galahad3x@users.noreply.github.com>
2023-08-10 10:58:30 +0200

    ballin

--- src/main/java/camp/CampManager/filter/CustomAuthenticationFilter.java
+++ src/main/java/camp/CampManager/filter/CustomAuthenticationFilter.java
@@ -3,6 +3,7 @@ package camp.CampManager.filter;
 import camp.CampManager.security.ConfigProperties;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,13 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -58,6 +66,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException {
         User user = (User) authentication.getPrincipal();
+        // TODO Aixo en teoria no funcione pero tampoc s'ha de fer servir
         assert secret != null;
         Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
         String accessToken = JWT.create()
@@ -81,5 +90,46 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
     public void setSecret(String secret) {
         this.secret = secret;
+    }
+
+    public String getTokenObjectFromKC(String username, String password) throws javax.naming.AuthenticationException, JsonProcessingException {
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(30))
+                .build();
+        Map<String, String> body = new HashMap<>();
+        body.put("username", username);
+        body.put("password", password);
+        body.put("client_id", "app");
+        body.put("grant_type", "password");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://keycloak-container:8080/realms/campmanager/protocol/openid-connect/token"))
+                .method("POST", getParamsUrlEncoded(body))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert response != null;
+        if (response.statusCode() != 200) {
+            throw new javax.naming.AuthenticationException();
+        }
+        Map<String, Object> result = new ObjectMapper().readValue(response.body(), HashMap.class);
+        if (result.containsKey("access_token")) {
+            return (String) result.get("access_token");
+        } else {
+            throw new javax.naming.AuthenticationException();
+        }
+    }
+
+    private HttpRequest.BodyPublisher getParamsUrlEncoded(Map<String, String> parameters) {
+        String urlEncoded = parameters.entrySet()
+                .stream()
+                .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+                .collect(Collectors.joining("&"));
+        return HttpRequest.BodyPublishers.ofString(urlEncoded);
     }
 }
--- src/main/java/camp/CampManager/filter/CustomAuthorizationFilter.java
+++ src/main/java/camp/CampManager/filter/CustomAuthorizationFilter.java
@@ -1,24 +1,36 @@
 package camp.CampManager.filter;
 
 import camp.CampManager.security.ConfigProperties;
+import camp.CampManager.security.UsersDetailsService;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.naming.AuthenticationException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -29,6 +41,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Autowired
     private ConfigProperties properties;
+    @Autowired
+    private UsersDetailsService usersDetailsService;
 
     private String secret;
 
@@ -43,16 +57,20 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                     String token = authorizationHeader.substring("Bearer ".length());
                     // TODO Aquí el servidor descodifica el JWT per a obtenir els rols
                     // Canviar això per agafar el JWT proporcionat pel Keycloak per treure la info de l'usuari
+                    String username = getUsernameFromKC(token);
                     // Fer un loadUserByUsername per a carregar les authorities
+                    User user = (User) usersDetailsService.loadUserByUsername(username);
+                    /* OLD
                     assert secret != null;
                     Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
                     JWTVerifier verifier = JWT.require(algorithm).build();
                     DecodedJWT decodedJWT = verifier.verify(token);
                     String username = decodedJWT.getSubject();
                     String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+                     */
                     // Crear authorities tal com estan aqui
                     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                    stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+                    user.getAuthorities().forEach(role -> authorities.add((SimpleGrantedAuthority) role));
                     UsernamePasswordAuthenticationToken authenticationToken =
                             new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
@@ -69,5 +87,39 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     public void setSecret(String secret) {
         this.secret = secret;
+    }
+
+    public Map<String, Object> getInfoFromKC(String token) throws AuthenticationException, JsonProcessingException {
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(30))
+                .build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://keycloak-container:8080/realms/campmanager/protocol/openid-connect/userinfo"))
+                .method("POST", HttpRequest.BodyPublishers.noBody())
+                .header("Authorization", "Bearer " + token)
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert response != null;
+        if (response.statusCode() != 200) {
+            throw new AuthenticationException();
+        }
+        return new ObjectMapper().readValue(response.body(), HashMap.class);
+    }
+
+    public String getUsernameFromKC(String token) throws AuthenticationException, JsonProcessingException {
+        var result = getInfoFromKC(token);
+        if (result.containsKey("given_name")) {
+            return (String) result.get("given_name");
+        }
+        if (result.containsKey("preferred_username")) {
+            return (String) result.get("preferred_username");
+        }
+        throw new AuthenticationException();
     }
 }
